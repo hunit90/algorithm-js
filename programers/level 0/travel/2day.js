8fc35e604a0361360824cbb99ba3fc1d4f89b017
hunit <137765835+hunit90@users.noreply.github.com>
2024-05-10 00:46:51 +0100

    2day

--- programers/level 0/travel/2day.js
+++ programers/level 0/travel/2day.js
@@ -29,8 +29,8 @@
  */
 
 function solution(angle) {
-    if (0 < angle < 90) return 1
-    else if (angle === 90) return 2
-    else if (90 < angle < 180) return 3
-    else if (angle === 180) return 4
+    if (0 < angle < 90) return 1;
+    else if (angle === 90) return 2;
+    else if (90 < angle < 180) return 3;
+    else if (angle === 180) return 4;
 }
